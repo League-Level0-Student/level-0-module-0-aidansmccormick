88b92aab403f46391358aaed6889331e49ae3081
League <league@WTS9.attlocal.net>
2018-02-01 19:55:25 -0800

    good dey

--- src/MyJavaProgram.java
+++ src/MyJavaProgram.java
@@ -1,13 +1,30 @@
 import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
+JOptionPane.showMessageDialog(null, "knock-knock");
+JOptionPane.showInputDialog("");
+JOptionPane.showMessageDialog(null, "knock-knock");
+JOptionPane.showInputDialog("");
+JOptionPane.showMessageDialog(null, "knock-knock");
+JOptionPane.showInputDialog("");
+JOptionPane.showMessageDialog(null, "knock-knock");
+JOptionPane.showInputDialog("");
+JOptionPane.showMessageDialog(null, "knock-knock");
+JOptionPane.showInputDialog("");
+JOptionPane.showMessageDialog(null, "knock-knock");
+JOptionPane.showInputDialog("");
+JOptionPane.showMessageDialog(null, "...");
+JOptionPane.showInputDialog("");
+JOptionPane.showMessageDialog(null, "glass shatters");
 	
-		
-		
+	
+	
 	}
 }
